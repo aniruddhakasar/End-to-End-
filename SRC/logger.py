f137b1114fe0e7ec07b0788a490712fa4e4c0103
aniruddhakasar <aniruddhakasar2001@gmail.com>
2023-05-01 21:54:54 +0530

    second

--- SRC/logger.py
+++ SRC/logger.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import datetime
+from SRC.logger import logging
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
